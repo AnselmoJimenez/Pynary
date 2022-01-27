df1fc2418ccb84a0148495ff861807f81b12ac9b
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-01-27 00:28:42 -0500

    Updated README.md and implemented pynary.py

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Pynary
-Binary converter in Python
+_Binary to Decimal and Decimal to Binary Converter in Python_ 
+
+## Purpose and Description
+This project was created to help with my Introduction to Computation Course at the Pennsylvania State University. We were tasked to create a Deterministic Finite Automata that accepted binary numbers that were divisible by 5. I simply used this to convert multiples of 5 to their binary representation and print them to the console.  
+
+---
+
+## Implementation
+To create this project I created the `Pynary` class with attributes **_decimal_** and **_binary_** that contains the setter and getter methods for the classes attributes, `decimal_to_binary(self, decimal)` and `binary_to_decimal(self, binary)`
+
+```Python
+decimal_to_binary(self, decimal):
+    binary = ""
+    while True:
+        if decimal >= 1:
+            binary += str(decimal % 2);
+            decimal = decimal // 2;
+        else:
+            break;
+    return binary[::-1];
+```
+* The Decimal to Binary method takes an unsigned integer, decimal, value and converts it to a binary string. We take this decimal value and find its remainder to 2 and then use integer division by 2 to assign decimal to the next significant bit. We do this until the decimal value becomes 0 and return the reverse the string we got.   
+Thanks to [geeksforgeeks](https://www.geeksforgeeks.org/program-decimal-binary-conversion/) for helping me get started with this method.  
+
+```Python
+def binary_to_decimal(self, binary):
+    decimal = 0;
+    for index, token in enumerate(reversed(binary)):
+        if token == "1" and decimal == 0: decimal = 1;
+        elif token == "1" and decimal != 0: decimal += 2**index;
+        else: continue;
+    self.decimal = decimal;
+```
+* The Binary to Decimal method takes in a string of binary numbers and converts it to its corresponding decimal value. We take this string value and read each value as well as its index in the string. The index will help us find the value we need to raise 2 to. If it is a 1 and it is the first iteration, we make decimal = 0. If it is not the first iteration then we add 2 to the power of the index to the current decimal value. if it is 0 then we just keep iterating until we find another 1 or the end of the string. Then we return the decimal value. 
--- pynary.py
+++ pynary.py
@@ -1,52 +1,55 @@
-from inspect import _void
-import string
-
-
 class Pynary:
-    def __init__(self):
-        pass;
-    
+    def __init__(self, decimal):
+        self.decimal = decimal;
+        self.binary = self.decimal_to_binary(decimal);
+
+    def get_decimal(self): 
+        return self.decimal;
+
+    def set_decimal(self, decimal):
+        self.decimal = decimal;
+
+    def get_binary(self):
+        return self.binary;
+
+    def set_binary(self, binary):
+        self.binary = binary;
+
     def decimal_to_binary(self, decimal):
-        if decimal >= 1:
-            self.decimal_to_binary(decimal // 2);
-        print(decimal % 2, end="");
+        binary = ""
+        while True:
+            if decimal >= 1:
+                binary += str(decimal % 2);
+                decimal = decimal // 2;
+            else:
+                break;
+        return binary[::-1];
+
+    def binary_to_decimal(self, binary):
+        decimal = 0;
+        for index, token in enumerate(reversed(binary)):
+            if token == "1" and decimal == 0: decimal = 1;
+            elif token == "1" and decimal != 0: decimal += 2**index;
+            else: continue;
+        self.decimal = decimal;
+
         
-            
-
-
-
 if __name__ == "__main__":
-    pynary = Pynary();
-    print("0: ", end="\t");
-    pynary.decimal_to_binary(0);
-    print();
-    print("5: ", end="\t");
-    pynary.decimal_to_binary(5);
-    print();
-    print("10: ", end="\t");
-    pynary.decimal_to_binary(10);
-    print();
-    print("15: ", end="\t");
-    pynary.decimal_to_binary(15);
-    print();
-    print("20: ", end="\t");
-    pynary.decimal_to_binary(20);
-    print();
-    print("25: ", end="\t");
-    pynary.decimal_to_binary(25);
-    print();
-    print("30: ", end="\t");
-    pynary.decimal_to_binary(30);
-    print();
-    print("35: ", end="\t");
-    pynary.decimal_to_binary(35);
-    print();
-    print("40: ", end="\t");
-    pynary.decimal_to_binary(40);
-    print();
-    print("45: ", end="\t");
-    pynary.decimal_to_binary(45);
-    print();
-    print("50: ", end="\t");
-    pynary.decimal_to_binary(50);
-    print();
+    pynary0 = Pynary(0);
+    print(f"0d: {pynary0.get_decimal()} \t\t 0b: {pynary0.get_binary()}");
+    pynary5 = Pynary(5);
+    print(f"5d: {pynary5.get_decimal()} \t\t 5b: {pynary5.get_binary()}");
+    pynary10 = Pynary(10);
+    print(f"10d: {pynary10.get_decimal()} \t 10b: {pynary10.get_binary()}");
+    pynary15 = Pynary(15);
+    print(f"15d: {pynary15.get_decimal()} \t 15b: {pynary15.get_binary()}");
+    pynary20 = Pynary(20);
+    print(f"20d: {pynary20.get_decimal()} \t 20b: {pynary20.get_binary()}");
+    pynary25 = Pynary(25);
+    print(f"25d: {pynary25.get_decimal()} \t 25b: {pynary25.get_binary()}");
+    pynary30 = Pynary(30);
+    print(f"30d: {pynary30.get_decimal()} \t 30b: {pynary30.get_binary()}");
+    pynary35 = Pynary(35);
+    print(f"35d: {pynary35.get_decimal()} \t 35b: {pynary35.get_binary()}");
+    pynary40 = Pynary(40);
+    print(f"40d: {pynary40.get_decimal()} \t 40b: {pynary40.get_binary()}");
